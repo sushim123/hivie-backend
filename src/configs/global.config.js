7cd65217e778bfcefe062d159b697df20c87e497
prabhootml <prabhootpatel@gmail.com>
2024-09-06 11:47:43 +0530

    fixed all

--- src/configs/global.config.js
+++ src/configs/global.config.js
@@ -8,13 +8,13 @@ export const OAUTH_CONFIG = {
   issuerBaseURL: process.env.OAUTH_ISSUER_BASE_URL
 };
 export const INSTA_TOKEN_PARAMS = {
-  client_id: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_ID : process.env.INSTA_CLIENT_ID_1,
-  client_secret: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_SECRET : process.env.INSTA_CLIENT_SECRET_1,
+  client_id: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_ID_1 : process.env.INSTA_CLIENT_ID,
+  client_secret: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_SECRET_1 : process.env.INSTA_CLIENT_SECRET,
   grant_type: 'authorization_code',
   redirect_uri: INSTA_REDIRECT_URL
 };
 export const INSTA_CODE_PARAMS = {
-  client_id: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_ID : process.env.INSTA_CLIENT_ID_1,
+  client_id: process.env.NODE_DEV_ENV ? process.env.INSTA_CLIENT_ID_1 : process.env.INSTA_CLIENT_ID,
   redirect_uri: INSTA_REDIRECT_URL,
   response_type: 'code',
   scope: 'user_profile,user_media'
