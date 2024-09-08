cd4c03bb31b0c34b1edb8a99b39bbfb08fc5747d
prabhootml <prabhootpatel@gmail.com>
2024-09-09 01:09:54 +0530

    modified route methods

--- src/routes/eCommerce.route.js
+++ src/routes/eCommerce.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import {
-  addFlashsaleProduct,
+  addCategoryName,
+  addFlashSaleProduct,
   addItemToCart,
+  addProduct,
   createCart,
   createOrder,
   createUser,
@@ -15,48 +17,47 @@ import {
   fetchAllUser,
   fetchbyId,
   fetchCategoryById,
-  fetchFlashsaleProductById,
+  fetchFlashSaleProductById,
   fetchOrderById,
   fetchOrderByUserId,
   fetchProductById,
-  getAllFlashsaleProduct,
+  getAllFlashSaleProduct,
   updateOrderStatus,
-  updateProduct,
   updateUser,
   updateUserAddress
 } from '../controllers/eCommerce.controller.js';
-import { asyncHandler } from '../utils/asyncHandler.util.js';
+import {asyncHandler} from '../utils/asyncHandler.util.js';
 const router = express.Router();
 // User Routes
 router.post('/user', asyncHandler(createUser));
-router.get('/user', asyncHandler(fetchAllUser  ));
-router.get('/user/:id',asyncHandler( fetchbyId));
+router.get('/user', asyncHandler(fetchAllUser));
+router.get('/user/:id', asyncHandler(fetchbyId));
 router.put('/user/:id', asyncHandler(updateUser));
 router.delete('/user/:id', asyncHandler(deleteUser));
 //address Routes
 router.post('/user/:id/address', asyncHandler(createUserAddress));
 router.get('/user/:id/address', asyncHandler(fetchAddressById));
 router.put('/address/:addressId', asyncHandler(updateUserAddress));
-router.delete('/address/:addressId',asyncHandler(deleteUSerAddress) );
+router.delete('/address/:addressId', asyncHandler(deleteUSerAddress));
 //order Routes
 router.post('/order', asyncHandler(createOrder));
 router.get('/order/:userId', asyncHandler(fetchOrderByUserId));
-router.get('/order/:id',asyncHandler(fetchOrderById) );
-router.patch('/order/:id/status',asyncHandler(updateOrderStatus) );
+router.get('/order/:id', asyncHandler(fetchOrderById));
+router.patch('/order/:id/status', asyncHandler(updateOrderStatus));
 //cart Routes
-router.post('/cart',asyncHandler(createCart) );
-router.post('/cart/item',asyncHandler(addItemToCart ) );
-router.get('/:cart_id/item',asyncHandler(fetchAllItemsInCart) );
+router.post('/cart', asyncHandler(createCart));
+router.post('/cart/item', asyncHandler(addItemToCart));
+router.get('/:cart_id/item', asyncHandler(fetchAllItemsInCart));
 //flasesale Routes
-router.post('/flashsale',asyncHandler(addFlashsaleProduct) );
-router.get('/flashsale',asyncHandler(getAllFlashsaleProduct ) );
-router.get('/flashsale/:id',asyncHandler(fetchFlashsaleProductById) );
+router.post('/flashSale', asyncHandler(addFlashSaleProduct));
+router.get('/flashSale', asyncHandler(getAllFlashSaleProduct));
+router.get('/flashSale/:id', asyncHandler(fetchFlashSaleProductById));
 //product Routes
-router.get('/product',asyncHandler(fetchAllProducts) );
-router.put('/product',asyncHandler(updateProduct ) );
-router.get('/product/:id',asyncHandler(fetchProductById) );
+router.post('/product', asyncHandler(addProduct));
+router.get('/product/:id', asyncHandler(fetchProductById));
+router.get('/product', asyncHandler(fetchAllProducts));
 //category Routes
-router.put('/category',asyncHandler(updateProduct) );
-router.get('/category',asyncHandler(fetchAllCategory) );
-router.get('/category/:id',asyncHandler(fetchCategoryById ) );
+router.post('/category', asyncHandler(addCategoryName));
+router.get('/category', asyncHandler(fetchAllCategory));
+router.get('/category/:id', asyncHandler(fetchCategoryById));
 export default router;
