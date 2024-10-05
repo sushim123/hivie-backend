be8fcabd1244e98c0e1d240a35f1e6e0fe60d6b5
sushim123 <sushimpaddd@gmail.com>
2024-10-05 23:50:48 +0530

    changes url in swagger

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000',
+        url: process.env.BASE_URL,
         description: 'Local server',
       },
     ],
